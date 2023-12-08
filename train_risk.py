b931425597af4da04fdd9eaf96416a027258f86b
Divya Thuremella <dthuremella@robots.ox.ac.uk>
2023-12-08 00:57:22 +0000

    add average metrics wandb

--- train_risk.py
+++ train_risk.py
@@ -286,7 +286,12 @@ def main():
     for epoch in range(1, args.train_epochs + 1):
         model_registrar.to(args.device)
         train_dataset.augment = args.augment
+        train_losses = {}
+        eval_losses_on_train = {}
         for node_type, data_loader in train_data_loader.items():
+            if node_type not in train_losses.keys():
+                train_losses[node_type] = []
+                eval_losses_on_train[node_type] = []
             curr_iter = curr_iter_node_type[node_type]
             pbar = tqdm(data_loader, ncols=80)
             for batch in pbar:
@@ -317,6 +322,8 @@ def main():
                 eval_loss_for_train_batch = eval_trajectron.eval_loss(batch, node_type)
                 wandb.log({"train loss {}".format(node_type): train_loss.item()})
                 wandb.log({"eval loss on train set {}".format(node_type): eval_loss_for_train_batch.item()})
+                train_losses[node_type].append(train_loss.item())
+                eval_losses_on_train[node_type].append(eval_loss_for_train_batch.item())
 
                 curr_iter += 1
             curr_iter_node_type[node_type] = curr_iter
@@ -326,6 +333,9 @@ def main():
 
         # WANDB: record epoch
         wandb.log({"epoch": epoch})
+        for node_type in train_losses.keys():
+            wandb.log({"avg train loss {}".format(node_type): np.mean(train_losses[node_type])})
+            wandb.log({"avg eval loss on train set {}".format(node_type): np.mean(eval_losses_on_train[node_type])})
 
         #################################
         #        VISUALIZATION          #
@@ -416,7 +426,12 @@ def main():
             model_registrar.to(args.eval_device)
             with torch.no_grad():
                 # Calculate evaluation loss
+                eval_losses = {}
+                train_losses_on_val_set = {}
                 for node_type, data_loader in eval_data_loader.items():
+                    if node_type not in eval_losses.keys():
+                        eval_losses[node_type] = []
+                        train_losses_on_val_set[node_type] = []
                     eval_loss = []
                     print(f"Starting Evaluation @ epoch {epoch} for node type: {node_type}")
                     pbar = tqdm(data_loader, ncols=80)
@@ -425,7 +440,9 @@ def main():
                         train_loss_node_type = trajectron.train_loss(batch, node_type, heatmap_tensor_ped, heatmap_tensor_veh, grid_tensor, 
                                                             loc_risk=args.location_risk, no_stat=args.no_stationary)
                         wandb.log({"train loss on val set {}".format(node_type): train_loss_node_type.item()})
-                        wandb.log({"eval loss{}".format(node_type): eval_loss_node_type.item()})
+                        train_losses_on_val_set[node_type].append(train_loss_node_type.item())
+                        wandb.log({"eval loss {}".format(node_type): eval_loss_node_type.item()})
+                        eval_losses[node_type].append(eval_loss_node_type.item())
                         pbar.set_description(f"Epoch {epoch}, {node_type} L: {eval_loss_node_type.item():.2f}")
                         eval_loss.append({node_type: {'nll': [eval_loss_node_type]}})
                         del batch
@@ -434,6 +451,9 @@ def main():
                                                 log_writer,
                                                 f"{node_type}/eval_loss",
                                                 epoch)
+                for node_type in eval_losses.keys():
+                    wandb.log({"avg train loss on val set {}".format(node_type): np.mean(train_losses_on_val_set[node_type])})
+                    wandb.log({"avg eval loss {}".format(node_type): np.mean(eval_losses[node_type])})
 
                 # Predict batch timesteps for evaluation dataset evaluation
                 eval_batch_errors = []
