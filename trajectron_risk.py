9ffcbf482c36e83796fc1f3571e586177483950d
Divya Thuremella <dthuremella@robots.ox.ac.uk>
2024-03-15 21:38:45 +0000

    set up ensemble with all the same model with possible aggregation strategies hardcoded variables

--- trajectron_risk.py
+++ trajectron_risk.py
@@ -7,6 +7,8 @@ from model.dataset.preprocessing import restore
 from mgcvae_risk import MultimodalGenerativeCVAERisk
 from preprocessing_risk import get_timesteps_data
 
+NUM_ENSEMBLE = [0,1,2] 
+aggregation_func = torch.mean
 
 class TrajectronRisk(Trajectron):
 
@@ -16,16 +18,37 @@ class TrajectronRisk(Trajectron):
         self.node_models_dict.clear()
         edge_types = env.get_edge_types()
 
-        for node_type in env.NodeType:
-            # Only add a Model for NodeTypes we want to predict
-            if node_type in self.pred_state.keys():
-                self.node_models_dict[node_type] = MultimodalGenerativeCVAERisk(env,
-                                                                            node_type,
-                                                                            self.model_registrar,
-                                                                            self.hyperparams,
-                                                                            self.device,
-                                                                            edge_types,
-                                                                            log_writer=self.log_writer)
+        for ens_index in NUM_ENSEMBLE:
+            self.node_models_dict[ens_index] = {}
+            for node_type in env.NodeType:
+                # Only add a Model for NodeTypes we want to predict
+                if node_type in self.pred_state.keys():
+                    self.node_models_dict[ens_index][node_type] = MultimodalGenerativeCVAERisk(env,
+                                                                                node_type,
+                                                                                self.model_registrar,
+                                                                                self.hyperparams,
+                                                                                self.device,
+                                                                                edge_types,
+                                                                                log_writer=self.log_writer)
+
+    def set_curr_iter(self, curr_iter):
+        self.curr_iter = curr_iter
+        for ens_str, node_dict in self.node_models_dict:
+            for node_str, model in node_dict.items():
+                model.set_curr_iter(curr_iter)
+
+    def set_annealing_params(self):
+        for ens_str, node_dict in self.node_models_dict:
+            for node_str, model in node_dict.items():
+                model.set_annealing_params()
+
+    def step_annealers(self, node_type=None):
+        for ens_index in NUM_ENSEMBLE:
+            if node_type is None:
+                for node_type in self.node_models_dict:
+                    self.node_models_dict[ens_index][node_type].step_annealers()
+            else:
+                self.node_models_dict[ens_index][node_type].step_annealers()
 
     def train_loss(self, batch, node_type, heatmap_tensor, grid_tensor, loc_risk=False, no_stat=False):
         (first_history_index,
@@ -52,29 +75,30 @@ class TrajectronRisk(Trajectron):
         if type(map) == torch.Tensor:
             map = map.to(self.device)
 
-        # Run forward pass
-        model = self.node_models_dict[node_type]
-        loss = model.train_loss(inputs=x,
-                                inputs_st=x_st_t,
-                                first_history_indices=first_history_index,
-                                labels=y,
-                                labels_st=y_st_t,
-                                neighbors=restore(neighbors_data_st),
-                                neighbors_edge_value=restore(neighbors_edge_value),
-                                robot=robot_traj_st_t,
-                                map=map,
-                                prediction_horizon=self.ph,
-                                # ---------------- ADDED --------------
-                                heatmap_tensor=heatmap_tensor,
-                                x_unf=x_unf,
-                                map_name=map_name,
-                                grid_tensor=grid_tensor,
-                                loc_risk=loc_risk,
-                                no_stat=no_stat
-                                # -------------------------------------
-
-                                )
-        return loss
+        losses = []
+        for ens_index in NUM_ENSEMBLE: # for each model in ensemble
+            # Run forward pass
+            model = self.node_models_dict[ens_index][node_type]
+            loss = model.train_loss(inputs=x,
+                                    inputs_st=x_st_t,
+                                    first_history_indices=first_history_index,
+                                    labels=y,
+                                    labels_st=y_st_t,
+                                    neighbors=restore(neighbors_data_st),
+                                    neighbors_edge_value=restore(neighbors_edge_value),
+                                    robot=robot_traj_st_t,
+                                    map=map,
+                                    prediction_horizon=self.ph,
+                                    heatmap_tensor=heatmap_tensor,
+                                    x_unf=x_unf,
+                                    map_name=map_name,
+                                    grid_tensor=grid_tensor,
+                                    loc_risk=loc_risk,
+                                    no_stat=no_stat
+                                    )
+            losses.append(loss)
+        ret = aggregation_func(torch.stack(losses))
+        return ret
 
     def eval_loss(self, batch, node_type):
         (first_history_index,
@@ -98,20 +122,23 @@ class TrajectronRisk(Trajectron):
         if type(map) == torch.Tensor:
             map = map.to(self.device)
 
-        # Run forward pass
-        model = self.node_models_dict[node_type]
-        nll = model.eval_loss(inputs=x,
-                              inputs_st=x_st_t,
-                              first_history_indices=first_history_index,
-                              labels=y,
-                              labels_st=y_st_t,
-                              neighbors=restore(neighbors_data_st),
-                              neighbors_edge_value=restore(neighbors_edge_value),
-                              robot=robot_traj_st_t,
-                              map=map,
-                              prediction_horizon=self.ph)
-
-        return nll.cpu().detach().numpy()
+        nlls = []
+        for ens_index in NUM_ENSEMBLE: # for each model in ensemble
+            # Run forward pass
+            model = self.node_models_dict[ens_index][node_type]
+            nll = model.eval_loss(inputs=x,
+                                inputs_st=x_st_t,
+                                first_history_indices=first_history_index,
+                                labels=y,
+                                labels_st=y_st_t,
+                                neighbors=restore(neighbors_data_st),
+                                neighbors_edge_value=restore(neighbors_edge_value),
+                                robot=robot_traj_st_t,
+                                map=map,
+                                prediction_horizon=self.ph)
+            nlls.append(nll)
+        ret = aggregation_func(torch.stack(nlls))
+        return ret.cpu().detach().numpy()
 
     def predict(self,
                 scene,
@@ -125,57 +152,61 @@ class TrajectronRisk(Trajectron):
                 full_dist=True,
                 all_z_sep=False):
 
-        predictions_dict = {}
-        for node_type in self.env.NodeType:
-            if node_type not in self.pred_state:
-                continue
+        all_models_predictions = []
+        for ens_index in NUM_ENSEMBLE:
+            for node_type in self.env.NodeType:
+                if node_type not in self.pred_state:
+                    continue
 
-            model = self.node_models_dict[node_type]
+                model = self.node_models_dict[ens_index][node_type]
 
-            # Get Input data for node type and given timesteps
-            batch = get_timesteps_data(env=self.env, scene=scene, t=timesteps, node_type=node_type, state=self.state,
-                                       pred_state=self.pred_state, edge_types=model.edge_types,
-                                       min_ht=min_history_timesteps, max_ht=self.max_ht, min_ft=min_future_timesteps,
-                                       max_ft=min_future_timesteps, hyperparams=self.hyperparams)
-            # There are no nodes of type present for timestep
-            if batch is None:
-                continue
-            (first_history_index,
-             x_t, y_t, x_st_t, y_st_t,
-             neighbors_data_st,
-             neighbors_edge_value,
-             robot_traj_st_t,
-             map,
-            #--------------ADDED--------------
-             x_unf_t,
-             map_name
-            #---------------------------------
-            ), nodes, timesteps_o = batch
+                # Get Input data for node type and given timesteps
+                batch = get_timesteps_data(env=self.env, scene=scene, t=timesteps, node_type=node_type, state=self.state,
+                                        pred_state=self.pred_state, edge_types=model.edge_types,
+                                        min_ht=min_history_timesteps, max_ht=self.max_ht, min_ft=min_future_timesteps,
+                                        max_ft=min_future_timesteps, hyperparams=self.hyperparams)
+                # There are no nodes of type present for timestep
+                if batch is None:
+                    continue
+                (first_history_index,
+                x_t, y_t, x_st_t, y_st_t,
+                neighbors_data_st,
+                neighbors_edge_value,
+                robot_traj_st_t,
+                map,
+                #--------------ADDED--------------
+                x_unf_t,
+                map_name
+                #---------------------------------
+                ), nodes, timesteps_o = batch
 
-            x = x_t.to(self.device)
-            x_st_t = x_st_t.to(self.device)
-            if robot_traj_st_t is not None:
-                robot_traj_st_t = robot_traj_st_t.to(self.device)
-            if type(map) == torch.Tensor:
-                map = map.to(self.device)
+                x = x_t.to(self.device)
+                x_st_t = x_st_t.to(self.device)
+                if robot_traj_st_t is not None:
+                    robot_traj_st_t = robot_traj_st_t.to(self.device)
+                if type(map) == torch.Tensor:
+                    map = map.to(self.device)
 
-            # Run forward pass
-            predictions = model.predict(inputs=x,
-                                        inputs_st=x_st_t,
-                                        first_history_indices=first_history_index,
-                                        neighbors=neighbors_data_st,
-                                        neighbors_edge_value=neighbors_edge_value,
-                                        robot=robot_traj_st_t,
-                                        map=map,
-                                        prediction_horizon=ph,
-                                        num_samples=num_samples,
-                                        z_mode=z_mode,
-                                        gmm_mode=gmm_mode,
-                                        full_dist=full_dist,
-                                        all_z_sep=all_z_sep)
+                # Run forward pass
+                predictions = model.predict(inputs=x,
+                                            inputs_st=x_st_t,
+                                            first_history_indices=first_history_index,
+                                            neighbors=neighbors_data_st,
+                                            neighbors_edge_value=neighbors_edge_value,
+                                            robot=robot_traj_st_t,
+                                            map=map,
+                                            prediction_horizon=ph,
+                                            num_samples=num_samples,
+                                            z_mode=z_mode,
+                                            gmm_mode=gmm_mode,
+                                            full_dist=full_dist,
+                                            all_z_sep=all_z_sep)
+                all_models_predictions.append(predictions)
 
-            predictions_np = predictions.cpu().detach().numpy()
+            aggregated_ensemble_predictions = aggregation_func(torch.stack(all_models_predictions), dim=0)
+            predictions_np = aggregated_ensemble_predictions.cpu().detach().numpy()
 
+            predictions_dict = {}
             # Assign predictions to node
             for i, ts in enumerate(timesteps_o):
                 if ts not in predictions_dict.keys():
@@ -201,8 +232,9 @@ class TrajectronRisk(Trajectron):
             if node_type not in self.pred_state:
                 continue
 
-            model = self.node_models_dict[node_type]
-            #import pdb; pdb.set_trace()
+            ens_index = NUM_ENSEMBLE[0]
+            model = self.node_models_dict[ens_index][node_type]
+
             # Get Input data for node type and given timesteps
             batch = get_timesteps_data(env=self.env, scene=scene, t=timesteps, node_type=node_type, state=self.state,
                                        pred_state=self.pred_state, edge_types=model.edge_types,
@@ -232,6 +264,6 @@ class TrajectronRisk(Trajectron):
                 vel_norm = np.linalg.norm(vel_stack.cpu(), axis=-1)
                 vel_norm = vel_norm[~np.isnan(vel_norm)]
                 vel_array = np.append(vel_array, vel_norm[:])
-            #import pdb; pdb.set_trace()
+
             average_batch_vel = np.mean(vel_array)
         return average_batch_vel
