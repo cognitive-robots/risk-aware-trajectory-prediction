8f3b1cd2841bf586088c51d27f0b4f9f37b9773c
Divya Thuremella <dthuremella@robots.ox.ac.uk>
2024-10-03 19:41:39 +0100

    make map work with model save fix

--- train_risk.py
+++ train_risk.py
@@ -302,7 +302,7 @@ def main():
             feat_dim[node_type] = hyperparams['map_encoder'][node_type]['output_size']
         input_multiplier = 1 if 'cluster' in args.ensemble_method else num_models
         input_dims = feat_dim if 'cluster' in args.ensemble_method else x_size
-        aggregation_model = create_stacking_model(train_env, input_dims, 
+        aggregation_model = create_stacking_model(train_env, model_registrar, input_dims, 
                                                   args.device, input_multiplier, num_models)
 
     trajectron.set_aggregation(args.ensemble_method,
